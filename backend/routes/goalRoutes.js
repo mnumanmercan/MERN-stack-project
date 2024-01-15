589281a26c4718b60afab51877c711f812463adf
Numan <numan.mercan24@gmail.com>
2024-01-16 00:00:58 +0300

    chore: goals controller and routes

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controllers/goalController");
+
+// router.get("/", getGoals);
+// router.post("/", setGoal);
+// @trick could be more minimal
+
+router.route('/').get(getGoals).post(setGoal); 
+
+router.route('/:id').put(updateGoal).delete(deleteGoal);
+
+module.exports = router;
