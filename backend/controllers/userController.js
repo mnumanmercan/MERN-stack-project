d2170119dcb4e60d789c16baf598663c6501af14
Numan <numan.mercan24@gmail.com>
2024-01-22 02:16:58 +0300

    chore: jwt token generation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,6 +39,7 @@ const registerUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       age: user.age,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -61,12 +62,12 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
     throw new Error("Invalid credentials");
   }
-
 });
 
 // @desc    Get user DATA
@@ -75,6 +76,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getMe = asyncHandler(async (req, res) => {
   res.json({ message: "User data" });
 });
+
+// Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
 
 module.exports = {
   registerUser,
