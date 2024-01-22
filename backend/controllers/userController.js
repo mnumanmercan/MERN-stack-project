ba9a84ce4524740c117b4a83fc7c245c92f43ba7
Numan <numan.mercan24@gmail.com>
2024-01-22 14:46:01 +0300

    chore: auth middleware with jwt token and protecting route process

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -72,9 +72,14 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc    Get user DATA
 // @route   GET /api/users/me
-// @access  Public
+// @access  Private
 const getMe = asyncHandler(async (req, res) => {
-  res.json({ message: "User data" });
+  const { _id, name, email } = await User.findById(req.user.id);
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
 });
 
 // Generate JWT
