e226e71e54d80174f9163e1686930899fb34a86e
Numan <numan.mercan24@gmail.com>
2024-01-23 21:41:22 +0300

    chore: delete and update user goals

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controllers/goalController");
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require("../controllers/goalController");
+
+const { protect } = require("../middleware/authMiddleware");
 
 // router.get("/", getGoals);
 // router.post("/", setGoal);
 // @trick could be more minimal
 
-router.route('/').get(getGoals).post(setGoal); 
+router.route("/").get(protect, getGoals).post(protect, setGoal);
 
-router.route('/:id').put(updateGoal).delete(deleteGoal);
+router.route("/:id").put(protect, updateGoal).delete(protect, deleteGoal);
 
 module.exports = router;
