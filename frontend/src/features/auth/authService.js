ee55e9804630c22af391ed082a2c58585419e5f1
Numan <numan.mercan24@gmail.com>
2024-01-27 00:49:08 +0300

    chore: redux login and logout  process

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -14,8 +14,27 @@ const register = async (userData) => {
   return response.data;
 };
 
+// Login user
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+
+  // User control and register to local storage
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+
+  return response.data;
+};
+
+// Logout user
+const logout = async () => {
+  localStorage.removeItem("user");
+};
+
 const authService = {
   register,
+  login,
+  logout,
 };
 
 export default authService;
