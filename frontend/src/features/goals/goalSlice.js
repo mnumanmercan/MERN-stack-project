9c8f1ceb97b16e1024d86657a6aac8e807da57c1
Numan <numan.mercan24@gmail.com>
2024-01-30 20:39:46 +0300

    chore: completed delete goals process

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -47,6 +47,25 @@ export const getGoals = createAsyncThunk(
   }
 );
 
+// Delete user goal
+export const deleteGoal = createAsyncThunk(
+  "goals/delete",
+  async (id, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState().auth.user.token;
+      return await goalService.deleteGoal(id, token);
+    } catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
 export const goalSlice = createSlice({
   name: "goal",
   initialState,
@@ -67,7 +86,7 @@ export const goalSlice = createSlice({
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
-      })  //Get goals extra reducers
+      }) //Get goals extra reducers
       .addCase(getGoals.pending, (state) => {
         state.isLoading = true;
       })
@@ -77,6 +96,21 @@ export const goalSlice = createSlice({
         state.goals = action.payload;
       })
       .addCase(getGoals.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      }) // Delete user goal with goalId
+      .addCase(deleteGoal.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(deleteGoal.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.goals = state.goals.filter(
+          (goal) => goal._id !== action.payload.id
+        );
+      })
+      .addCase(deleteGoal.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = true;
         state.message = action.payload;
